c2bb0bf9f5b69e03b4e109b37ed512430e8d84c6
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2022-05-26 07:35:45 +0200

    curl

--- build-rootfs.py
+++ build-rootfs.py
@@ -12,9 +12,19 @@ with urllib.request.urlopen(f'http://landley.net/toybox/bin/toybox-{platform.mac
     toybox = f.read()
     with open('rootfs/bin/toybox', 'wb') as file:
         file.write(toybox)
-        os.chmod('rootfs/bin/toybox', 0o777)
-    toybox_commands = subprocess.run(['rootfs/bin/toybox', '--long'], capture_output=True, text=True).stdout.split()
-    print(toybox_commands)
-    for c in toybox_commands:
-        print(c)
-        os.symlink('/bin/toybox', f'rootfs/{c}')
+os.chmod('rootfs/bin/toybox', 0o777)
+toybox_commands = subprocess.run(['rootfs/bin/toybox', '--long'], capture_output=True, text=True).stdout.split()
+print(toybox_commands)
+for c in toybox_commands:
+    print(c)
+    os.symlink('/bin/toybox', f'rootfs/{c}')
+
+curl_arch_mapping = {
+    'x86_64': 'amd64',
+    'aarch64': 'aarch64'
+}
+with urllib.request.urlopen(f'https://github.com/moparisthebest/static-curl/releases/latest/download/curl-{curl_arch_mapping[platform.machine()]}') as f:
+    curl = f.read()
+    with open('rootfs/bin/curl', 'wb') as file:
+        file.write(curl)
+os.chmod('rootfs/bin/curl', 0o777)
